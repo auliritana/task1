bfc989f646b59eb5bd29fb7cefb8bb477e62d767
Elena <isupova.elena@gmail.com>
2019-06-21 13:13:00 +0300

    get html elements from js

---
+++ task7/js/main.js
@@ -0,0 +1,41 @@
+let startBtn = document.getElementById("start"),
+    budgetValue = document.getElementsByClassName("budget-value")[0],
+    dayBudgetValue = document.getElementsByClassName("daybudget-value")[0],
+    levelValue = document.getElementsByClassName("level-value")[0],
+    expensesValue = document.getElementsByClassName("expenses-value")[0],
+    optionalExpensesValue = document.getElementsByClassName("optionalexpenses-value")[0],
+    incomeValue = document.getElementsByClassName("income-value")[0],
+    monthSavingsValue = document.getElementsByClassName("monthsavings-value")[0],
+    yearSavingsValue = document.getElementsByClassName("yearsavings-value")[0],
+    inputExpenses = document.getElementsByClassName("expenses-item"),
+    agreeBtn = document.getElementsByTagName("button")[0],
+    processBtn = document.getElementsByTagName("button")[1],
+    optionalExpenses = document.querySelectorAll(".optionalexpenses-item"),
+    posIncome = document.querySelector(".choose-income"),
+    savingsCheck = document.querySelector("#savings"),
+    savingsSum = document.querySelector(".choose-sum"),
+    savingsPercent = document.querySelector(".choose-percent"),
+    yearValue = document.getElementsByClassName("year-value")[0],
+    monthValue = document.getElementsByClassName("month-value")[0],
+    dayValue = document.getElementsByClassName("day-value")[0];
+
+console.log(startBtn);
+console.log(budgetValue);
+console.log(dayBudgetValue);
+console.log(levelValue);
+console.log(expensesValue);
+console.log(optionalExpensesValue);
+console.log(incomeValue);
+console.log(monthSavingsValue);
+console.log(yearSavingsValue);
+console.log(inputExpenses);
+console.log(agreeBtn);
+console.log(processBtn);
+console.log(optionalExpenses);
+console.log(posIncome);
+console.log(savingsCheck);
+console.log(savingsSum);
+console.log(savingsPercent);
+console.log(yearValue);
+console.log(monthValue);
+console.log(dayValue);
